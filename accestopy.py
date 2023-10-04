8eccd360d699e94baf7da5fddaa2ef13054acb5e
sb171732 <sauronkan@gmail.com>
2023-10-04 10:46:37 +0700

    add insert new record

--- accestopy.py
+++ accestopy.py
@@ -1,6 +1,10 @@
 import pyodbc
 conn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\\Users\\Admin\\Documents\\30A\\acces_to_py\\db.accdb;')
 cursor = conn.cursor() # создается курсор
+
+cursor.execute("insert into table1(name, fam) values('Сереп', 'Начын')")
+cursor.commit()
+
 cursor.execute("select *from table1")
 for string in cursor.fetchall():
   print(string)  # Выводятся строки базы данных.
